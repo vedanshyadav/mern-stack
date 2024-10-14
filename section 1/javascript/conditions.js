1d2a0d5b59695e518e909dc69ff5159ab935837d
yadavvedansh147@gmail.com <148164441+vedanshyadav@users.noreply.github.com>
2024-10-14 17:23:10 +0530

    javascirpt

---
+++ section 1/javascript/conditions.js
@@ -0,0 +1,31 @@
+a = 56;
+if(a%2==0){
+console.log("even");
+}
+else{
+    console.log("odd");
+    
+}
+
+// WAP to check if a perso0n os  eligible  for dl or not 
+
+age = 16;
+if(age<18){
+    abc="something"
+    const pi = 3.14;
+    console.log("not eligible");
+    console.log(a);
+    console.log(pi);
+    
+    //pi=3.15;
+    
+
+    
+}
+
+
+else{
+    console.log("eligible");
+    
+}
+console.log(abc);
--- section 1/javascript/index.js
+++ section 1/javascript/index.js
@@ -1,1 +1,11 @@
 console.log("hello Javascript");
+a=234;
+myname="vedansh yadav "
+
+console.log(a , myname);
+
+myname= "yadavji"
+
+console.log(myname);
+
+console.log([typeof myname]);
