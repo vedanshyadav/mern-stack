b2369704a70c09cf3f7dbbc71214f38039b63164
yadavvedansh147@gmail.com <148164441+vedanshyadav@users.noreply.github.com>
2024-10-23 23:16:09 +0530

    kuch bhi

--- section 1/javascript/conditions.js
+++ section 1/javascript/conditions.js
@@ -48,3 +48,53 @@ else{
     console.log("zero");
     
 }
+a = 56;
+if(a%2==0){
+console.log("even");
+}
+else{
+    console.log("odd");
+    
+}
+
+// WAP to check if a perso0n os  eligible  for dl or not 
+
+age = 16;
+if(age<18){
+    abc="something"
+    const pi = 3.14;
+    console.log("not eligible");
+    console.log(a);
+    console.log(pi);
+    
+    //pi=3.15;
+    
+
+    
+}
+
+
+else{
+    console.log("eligible");
+    
+}
+console.log(abc);
+//WAp to check if it is a number is positive or negtive or zero
+
+num=500;
+
+if(num>0){
+    console.log("positive");
+
+    
+}
+
+else if(num<0){
+    console.log("negative");
+    
+}
+
+else{
+    console.log("zero");
+    
+}
